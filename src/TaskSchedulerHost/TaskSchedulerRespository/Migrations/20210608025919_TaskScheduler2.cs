c6a0a450ad74cdecf031ad9a2f10527407cae09d
lkl <lhzcm888888@gmail.com>
2021-06-08 14:10:05 +0800

    添加了迁移代码文件

---
+++ src/TaskSchedulerHost/TaskSchedulerRespository/Migrations/20210608025919_TaskScheduler2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskSchedulerRespository.Migrations
+{
+    public partial class TaskScheduler2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "WriteTime",
+                table: "t_log",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2021, 5, 24, 16, 29, 43, 127, DateTimeKind.Local).AddTicks(2217)
+                );
+                
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "WriteTime",
+                table: "t_log",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2021, 5, 24, 16, 29, 43, 127, DateTimeKind.Local).AddTicks(2217),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+        }
+    }
+}
