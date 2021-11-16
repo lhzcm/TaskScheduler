640fdc38472094f9d29a9e7214132343a7a4ef46
lkl <lhzcm888888@gmail.com>
2021-11-16 09:53:43 +0800

    提交了没有上传的代码

---
+++ src/TaskSchedulerHost/TaskSchedulerHost/Extensions/CacheExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskSchedulerHost.Utility;
+using TaskSchedulerModel.Models;
+
+namespace TaskSchedulerHost.Extensions
+{
+    public static class CacheExtensions
+    {
+        public static UserInfo GetUser(this IMemoryCache Cache, int Userid)
+        {
+            return CacheUtility.GetUser(Userid, Cache);
+        }
+
+        public static void SetUser(this IMemoryCache Cache, UserInfo User)
+        {
+            CacheUtility.SetUser(User, Cache);
+        }
+    }
+}
